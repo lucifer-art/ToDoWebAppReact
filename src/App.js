3144b065874a71a7c473604b9860579b454086bd
Shivang Kumar <shivang.kumar@deltaschool.in>
2021-06-15 19:49:36 +0530

    added task handler

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import { Fragment } from 'react';
+import { Fragment,useState } from 'react';
 import TaskForm from './components/containers/TaskForm/TaskForm';
 import Header from './components/UI/Header/Header';
 
 function App() {
+
+  const [taskList, settaskList] = useState([]);
+
+  const taskListHandler = taskdata => {
+    settaskList(task =>{
+      return [...task, taskdata]
+    });
+    console.log(taskList);
+  }
+
   return (
     <Fragment>
       <Header />
-      <TaskForm />
+      <TaskForm taskList={taskListHandler} />
     </Fragment>
   );
 }
--- src/components/containers/TaskForm/TaskForm.js
+++ src/components/containers/TaskForm/TaskForm.js
@@ -1,18 +1,44 @@
+import { useState } from 'react';
 import classes from "./TaskForm.module.css";
 import Card from "../../UI/Card/Card";
 
 const TaskForm = props => {
+
+    const [addTask, setAddTask] = useState('');
+    const [addPriority, setaddPriority] = useState('');
+
+    const addTaskHandler = event => {
+        event.preventDefault();
+        setAddTask(event.target.value);
+    }
+    const addPriorityHandler = event => {
+        event.preventDefault();
+        setaddPriority(event.target.value);
+    }
+
+    const onSubmitHandler = event => {
+        event.preventDefault();
+        const task = {
+            id:Math.random() / 100,
+            task: addTask,
+            priority: addPriority
+        }
+        props.taskList(task);
+        setAddTask('');
+        setaddPriority('');
+    }
+
     return (
         <Card>
-            <form>
+            <form onSubmit={onSubmitHandler}>
                 <div className={classes.formControl}>
                     <label htmlFor='task'>Task</label>
-                    <input id='task' type="text" placeholder="Provide task" />
+                    <input id='task' value={addTask} type="text" onChange={addTaskHandler} placeholder="Provide task" />
                 </div>
                 <div className={classes.formControl}>
                     <label htmlFor='taskPriority'>Task Priority</label>
-                    <select>
-                        <option id='taskPriority' value=''>Select Priority</option>
+                    <select id='taskPriority' onChange={addPriorityHandler} value={addPriority}>
+                        <option value=''>Select Priority</option>
                         <option value='Low'>Low</option>
                         <option value='Moderate'>Moderate</option>
                         <option value='High'>High</option>
